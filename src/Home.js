af7362013b4f078b6fdb362c6adde0c11c4c259c
NeginSal <neginsaljooghi@gmail.com>
2022-02-08 18:38:29 +0330

    add state

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,24 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  const [values, setValues] = useState({
-    firstName: "",
-    lastName: "",
-    phoneNumber: "",
-    phone: "",
-    address: "",
-  });
+    const [firstName, setfirstName]= useState('');
+    const [lastName, setLastName] = useState('');
+    const [Mobile, setMobile ]= useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+    const [gender, setGender] = useState('1');
+
+
   return (
     <div className="create">
       <h1>this is form page</h1>
       <form>
-        <input type="text" required placeholder="نام"></input>
-        <input type="text" required placeholder="نام خانوادگی"></input>
-        <input type="tel" required placeholder="تلفن همراه"></input>
-        <input type="tel" required placeholder="تلفن ثابت"></input>
-        <input type="text" required placeholder="آدرس دقیق"></input>
-        <select>
-          <option value="0" selected="selected">
-         جنسیت
-          </option>
+        <input type="text" required value={firstName} onChange={(e) => setfirstName(e.target.value)}  placeholder="نام"></input>
+        <input type="text" required value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="نام خانوادگی"></input>
+        <input type="tel" required value={Mobile} onChange={(e) => setMobile (e.target.value)} placeholder="تلفن همراه"></input>
+        <input type="tel" required value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="تلفن ثابت"></input>
+        <input type="text" required value={address} onChange={(e) => setAddress(e.target.value)} placeholder="آدرس دقیق"></input>
+        <select value={gender} onChange={(e) => setGender(e.target.value)} >
           <option value="1">خانم</option>
           <option value="2">آقا</option>
         </select>
